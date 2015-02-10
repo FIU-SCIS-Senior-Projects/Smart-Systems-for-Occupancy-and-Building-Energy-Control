b8fbeeab6a1ff9890849b2ba4907676a0c18dd06
Study01 <daly.hidalgo@hotmail.com>
2015-02-10 08:54:42 -0500

    Edited UserSQLiteDatabase

--- Code/SSOBEC/app/src/main/java/fiu/ssobec/UserSQLiteDatabase.java
+++ Code/SSOBEC/app/src/main/java/fiu/ssobec/UserSQLiteDatabase.java
@@ -14,12 +14,27 @@ public class UserSQLiteDatabase extends SQLiteOpenHelper {
     public static final String COLUMN_NAME = "user_name";
     public static final String COLUMN_EMAIL = "user_email";
 
+    private static final String DATABASE_NAME = "commments.db";
+    private static final int DATABASE_VERSION = 1;
+
+    //Database creation sql database
+    private static final String DATABASE_CREATE = "create table "
+            +TABLE_USER + "("
+            +COLUMN_ID + "int PRIMARY KEY, "
+            +COLUMN_NAME + "varchar(50) NOT NULL, "
+            +COLUMN_EMAIL + "varchar(50) NOT NULL "
+            +
+            ");";
+
+
+
     public UserSQLiteDatabase(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(DATABASE_CREATE);
 
     }
 
