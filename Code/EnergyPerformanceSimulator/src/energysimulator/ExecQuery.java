6491c8d973426621765b8564fad00426dfa62668
Maria Presa <mariaunia@gmail.com>
2015-02-17 14:17:51 -0300

    Created a new Java project that automatically submits data to the database and simulates energy behavior of a building

---
+++ Code/EnergyPerformanceSimulator/src/energysimulator/ExecQuery.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package energysimulator;
+
+/**
+ *
+ * @author Maria
+ */
+public class ExecQuery implements Runnable {
+
+    
+    
+    
+    ExecQuery(){
+        
+    }
+    
+    @Override
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
