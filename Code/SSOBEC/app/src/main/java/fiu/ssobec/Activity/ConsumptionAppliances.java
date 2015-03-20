10567e9f24447797bd0288629a607130c0376c1a
Study01 <daly.hidalgo@hotmail.com>
2015-03-19 23:15:05 -0400

    Working with Naïve Bayes Algorithm.

--- Code/SSOBEC/app/src/main/java/fiu/ssobec/Activity/ConsumptionAppliances.java
+++ Code/SSOBEC/app/src/main/java/fiu/ssobec/Activity/ConsumptionAppliances.java
@@ -1,9 +1,11 @@
-package fiu.ssobec;
+package fiu.ssobec.Activity;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import fiu.ssobec.R;
 
 
 public class ConsumptionAppliances extends ActionBarActivity {
