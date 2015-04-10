5cbed062653a6b07191b4e82fc9df54d1b763753
Study01 <daly.hidalgo@hotmail.com>
2015-04-10 14:55:04 -0400

    Working with the reward design

---
+++ Code/SSOBEC/app/src/main/java/fiu/ssobec/RewardListParent.java
@@ -0,0 +1,44 @@
+package fiu.ssobec;
+
+/**
+ * Created by Fresa on 4/10/2015.
+ */
+public class RewardListParent {
+
+    private String name;
+    private String description;
+    private String icon;
+    private String points;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public void setPoints(String points) {
+        this.points = points;
+    }
+}
