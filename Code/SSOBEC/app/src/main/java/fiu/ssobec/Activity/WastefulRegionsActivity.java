13c411ae13d9eef4a50de537efb4d4f57a6d12c5
dlean001 <dlean001@fiu.edu>
2015-06-25 23:18:21 -0400

    Added activities and layouts to turn off light and turn off appliance

--- Code/SSOBEC/app/src/main/java/fiu/ssobec/Activity/WastefulRegionsActivity.java
+++ Code/SSOBEC/app/src/main/java/fiu/ssobec/Activity/WastefulRegionsActivity.java
@@ -1,5 +1,6 @@
 package fiu.ssobec.Activity;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -13,6 +14,7 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,8 +22,11 @@ import fiu.ssobec.Adapters.MyRewardListAdapter;
 import fiu.ssobec.Adapters.WastefulRegionListAdapter;
 import fiu.ssobec.AdaptersUtil.RewardListParent;
 import fiu.ssobec.AdaptersUtil.WastefulRegionListParent;
+import fiu.ssobec.DataAccess.DataAccessUser;
 import fiu.ssobec.DataAccess.ExternalDatabaseController;
+import fiu.ssobec.Model.User;
 import fiu.ssobec.R;
+import fiu.ssobec.Synchronization.SyncUtils;
 
 /**
  * Created by diana on June 2015.
@@ -29,62 +34,95 @@ import fiu.ssobec.R;
 public class WastefulRegionsActivity extends ActionBarActivity {
     public static final String LOG_TAG = "WastefulRegionsActivity";
     public static final String GETWasteRegions_PHP = "http://smartsystems-dev.cs.fiu.edu/getwastefulregions.php";
+    private static DataAccessUser data_access;
+    public static final int USER_LOGGEDIN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.i(LOG_TAG, "Starting WastefulRegionsActivity");
-        List<NameValuePair> emptyarr = new ArrayList<>(1);
+        //Declare the access to the SQLite table for user
+        data_access = new DataAccessUser(this);
 
-        String res = null;
-        ArrayList<WastefulRegionListParent> wasteful_regions_list = new ArrayList<WastefulRegionListParent>();
-
-        //Get the regions that are currently wasteful based on plugload and lights on without occupants
+        //Open the data access to the tables
         try {
-            res = new ExternalDatabaseController((ArrayList<NameValuePair>) emptyarr, GETWasteRegions_PHP).send();
-
-            Log.i(LOG_TAG, "Result: " + res);
-        }catch(InterruptedException e){
+            data_access.open();
+        } catch (SQLException e) {
+            System.err.println(LOG_TAG + ": " + e.toString());
             e.printStackTrace();
         }
 
-        if (res != null) {
-            try{
-                JSONObject obj = new JSONObject(res);
-                JSONObject myobj;
-                int j = 0;
-                while (obj.has(j + "") && j < obj.length()) {
-                    myobj = obj.getJSONObject(j + "");
-                    System.out.println("length "+obj.length());
+        //Synchronize Data
+        SyncUtils.CreateSyncAccount(this);
+        SyncUtils.TriggerRefresh();
 
-                    String name = myobj.getString("region_name");
-                    String light_description = myobj.getString("description");
-                    double plugload = myobj.getDouble("plugload");
+        User user = data_access.getUser(USER_LOGGEDIN); //Get a user that is currently logged in the system
 
-                    WastefulRegionListParent record = new WastefulRegionListParent();
-                    record.setName(name);
-                    record.setLightDescription(light_description);
-                    record.setPlugload(plugload + "");
-                    wasteful_regions_list.add(record);
-
-                    j++;
-                }
-
-            }catch(JSONException e){
-                e.printStackTrace();
-            }
+        //If a user that is logged in into the system is not found then start a new LoginActivity
+        if (user == null) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
         }
 
-        setContentView(R.layout.activity_wasteful_regions);
+        //User that is currently logged in is found
+        else {
+            setContentView(R.layout.activity_wasteful_regions);
+            ArrayList<WastefulRegionListParent> wasteful_regions_list = getWastefulRegions();
 
-
-        ListView listview_wasteful_regions = (ListView) findViewById(R.id.wasteful_regions);
-        WastefulRegionListAdapter wastefulRegionsListAdapter = new WastefulRegionListAdapter(this);
-        wastefulRegionsListAdapter.setParents(wasteful_regions_list);
-        listview_wasteful_regions.setAdapter(wastefulRegionsListAdapter);
-
+            ListView listview_wasteful_regions = (ListView) findViewById(R.id.wasteful_regions);
+            WastefulRegionListAdapter wastefulRegionsListAdapter = new WastefulRegionListAdapter(this);
+            wastefulRegionsListAdapter.setParents(wasteful_regions_list);
+            listview_wasteful_regions.setAdapter(wastefulRegionsListAdapter);
+        }
     }
 
+private ArrayList<WastefulRegionListParent> getWastefulRegions(){
+    Log.i(LOG_TAG, "Starting WastefulRegionsActivity");
+    List<NameValuePair> emptyarr = new ArrayList<>(1);
+
+    String res = "";
+    ArrayList<WastefulRegionListParent> wasteful_regions_list = new ArrayList<WastefulRegionListParent>();
+
+    //Get the regions that are currently wasteful based on plugload and lights on without occupants
+    try {
+        res = new ExternalDatabaseController((ArrayList<NameValuePair>) emptyarr, GETWasteRegions_PHP).send();
+
+        Log.i(LOG_TAG, "Result: " + res);
+    } catch (InterruptedException e) {
+        e.printStackTrace();
+    }
+    Log.i(LOG_TAG, "res: " + res);
+    System.out.println("printout: " + res);
+    if (res != null) {
+        JSONObject obj;
+
+        try {
+            obj = new JSONObject(res);
+            JSONObject myobj;
+            int j = 0;
+            while (obj.has(j + "") && j < obj.length()) {
+                myobj = obj.getJSONObject(j + "");
+                System.out.println("length " + obj.length());
+
+                String name = myobj.getString("region_name");
+                String light_description = myobj.getString("description");
+                double plugload = myobj.getDouble("plugload");
+
+                WastefulRegionListParent record = new WastefulRegionListParent();
+                record.setName(name);
+                record.setLightDescription(light_description);
+                record.setPlugload(plugload + "");
+                wasteful_regions_list.add(record);
+
+                j++;
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
+
+    return wasteful_regions_list;
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
